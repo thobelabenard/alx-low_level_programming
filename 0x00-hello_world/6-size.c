2dbbb715421d26f45358bdc8d645f15026a37f5b
thebelabenard <thobelabbb@ymail.com>
2023-07-13 14:55:25 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - various types on the computer it is compiled
+ * return: 0
+ */
+int main(void)
+{
+	char j;
+	int b;
+	long int k;
+	long long int p;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", unsigned long)sizeof(j);
+printf("Size of a int: %lu byte(s)\n", unsigned long)sizeof(b);
+printf("Size of a long int: %lu bytes(s)\n", unsigned long)sizeof(k);
+printf("Size of a long long int: %lu bytes(s)\n", unsigned long)sizeof(p);
+printf("Size of a float: %lu bytes(s)\n", unsigned long)sizeof(f);
+return (0);
+}
